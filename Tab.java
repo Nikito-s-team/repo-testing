380f9966437d338cb0eeb75880e2e022f6e26e13
bone4kacat <59851866+bone4kacat@users.noreply.github.com>
2020-01-15 10:20:30 -0500

    Add files via upload

---
+++ Tab.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Tab {
+	public String ClassName;
+
+	public static void main(String[] args) {
+		System.out.println("Try 1");
+
+	}
+	public void setTestClass(String newClassName) {
+		ClassName=newClassName;
+	}
+
+}
